9121735a074b741c50dd2a81d93e1febc3ed2235
jackarms <90922918+jackarms@users.noreply.github.com>
2021-11-26 15:46:20 -0600

    added length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,17 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+// Get references to the #generate element, this var ties us to Generate Password button.
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  window.alert("Before we begin let's set a few parameters first.");
+  var length = prompt("Set a length for your password, must be between 8 and 128 characters.");
+  if (length > 8 && length < 128){alert('Excellent, your password is ' + length + ' characters long.')}
+  else if (length < 8 || length > 128){prompt('Set a length for your password, must be between 8 and 128 characters.')};
+
+  var passwordText = document.querySelector("#password")
 
   passwordText.value = password;
 
